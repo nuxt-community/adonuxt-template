3eccf95e0073a7ee8182d2b9333e79c21b7fec43
Zachary Volpi <z.volpi@infomir.com>
2018-05-10 10:56:21 +0200

    specify vuetify colors

--- resources/plugins/vuetify.js
+++ resources/plugins/vuetify.js
@@ -1,4 +1,15 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import colors from 'vuetify/es5/util/colors'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#121212',
+    accent: colors.grey.darken3,
+    secondary: colors.amber.darken3,
+    info: colors.teal.lighten1,
+    warning: colors.amber.darken2,
+    error: colors.deepOrange.accent4,
+    success: colors.green.accent3
+  }
+})
